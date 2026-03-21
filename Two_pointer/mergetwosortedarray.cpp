2f03874bd423a515f3677776aff74754237c5a7e
Mahir-verma <mahir.v2k13@gmail.com>
2026-03-22 00:00:16 +0530

    Implement merge function for two sorted arrays

---
+++ Two_pointer/mergetwosortedarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int n1=m-1;
+        int n2=n-1;
+        int pos=m+n-1;
+
+        while(n1>=0 && n2>=0){                          //Brute force to store the answer in the new ans vector of size m+n 
+            if(nums1[n1]<=nums2[n2]){
+                nums1[pos]=nums2[n2];
+                pos--;
+                n2--;
+            }else{
+                nums1[pos]=nums1[n1];
+                pos--;
+                n1--;
+            }
+        }
+        while(n2>=0){
+            nums1[pos]=nums2[n2];
+            pos--;
+            n2--;
+        }
+    }
+};
