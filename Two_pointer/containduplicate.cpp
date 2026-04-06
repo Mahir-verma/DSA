d056c82b624c48f659185af06b42548ceecc4bf3
Mahir-verma <mahir.v2k13@gmail.com>
2026-04-06 09:41:59 +0530

    Create containduplicate.cpp

---
+++ Two_pointer/containduplicate.cpp
@@ -0,0 +1,16 @@
+simple  sort or make hash table 
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return false;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
