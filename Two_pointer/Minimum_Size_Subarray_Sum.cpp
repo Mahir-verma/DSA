ed7ec903182d3a4e6c529e65e0ed8ad4b5c95889
Mahir-verma <mahir.v2k13@gmail.com>
2026-04-07 16:33:56 +0530

    Update Minimum_Size_Subarray_Sum.cpp

--- Two_pointer/Minimum_Size_Subarray_Sum.cpp
+++ Two_pointer/Minimum_Size_Subarray_Sum.cpp
@@ -1,6 +1,11 @@
 // problem i was facing in this problem was that i was shrinking it before adding the element 
 // basically my main problem was that the way i was iterating 
 
+// concept
+// add right
+// while sum >= target:
+//     update answer
+//     shrink from left
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
@@ -21,3 +26,10 @@ public:
         return (len == INT_MAX) ? 0 : len;
     }
 };
+// At each r:
+
+// include nums[r]
+// if window is valid, try to shrink it as much as possible
+// update minimum length before removing from left
+
+// That guarantees the shortest valid window ending at r.
